cb9eb8330146c912967fefbbda4104f9b30e7629
Yann Duran <yd@live.com.au>
2017-11-15 01:59:31 +1100

    Implement basic list behavior

---
+++ src/pkg/UI/PathVariables/ItemData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+using static System.Environment;
+
+namespace Luminous.TimeSavers.UI.PathVariables
+{
+    public class ItemData : ObservableCollection<Model>
+    {
+        public ItemData()
+        {
+            const char semi_colon = ';';
+            var variables = ExpandEnvironmentVariables("%path%");
+            var values = variables.Split(semi_colon).ToList();
+
+            values.ForEach((s) => Add(new Model() { Caption = s }));
+        }
+
+        public void Load()
+        {
+
+        }
+    }
+}
--- src/pkg/UI/PathVariables/ViewModel.cs
+++ src/pkg/UI/PathVariables/ViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace Luminous.TimeSavers.UI.PathVariables
+﻿using System.Collections.ObjectModel;
+
+namespace Luminous.TimeSavers.UI.PathVariables
 {
     public class ViewModel : ViewModelBase
     {
+        public ObservableCollection<Model> Items { get; private set; }
+
+        public ViewModel()
+        {
+            Items = new ItemData();
+        }
+
     }
 }

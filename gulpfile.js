7863e40bd5149bf5579c2d8baedce70094029321
Yann Duran <yd@live.com.au>
2017-12-19 22:57:58 +1100

    Update mkdocs configuration files

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 /// <binding AfterBuild='build' />
 'use strict';
 
-var gulp = require('gulp');
-var gat = require('gulp-all-tasks')();
-var ghPages = require('gulp-gh-pages');
-var exec = require('child_process').exec;
-//var exec = require('gulp-exec');
+const gulp = require('gulp');
+const gat = require('gulp-all-tasks')();
+const exec = require('child_process').exec;
+const opn = require('opn');
 
-var config = {
+const config = {
     repo: 'https://github.com/luminous-software/time-savers.git',
     remote: 'github',
     branch: 'master',
@@ -18,12 +17,14 @@ var config = {
     user: {
         email: 'yd@live.com.au',
         name: 'Yann Duran'
-    }
+    },
+    address: '127.0.0.1',
+    port: '8004'
 }
 
-var script = {
+const script = {
     build: 'mkdocs build',
-    serve: 'mkdocs serve --dev-addr=127.0.0.1:8003',
+    serve: 'mkdocs serve --dev-addr=' + config.address + ':' + config.port,
     //deploy: 'mkdocs gh_pages',
 
     changes: 'git log -n 1 HEAD --pretty=format:"  * %s"',
@@ -32,34 +33,38 @@ var script = {
 
 gulp.task('log', function (cb) {
     exec(script.log, function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
+        gulp_util.log(stdout);
+        gulp_util.log(stderr);
         cb(err);
-    })
+    });
 });
 
 gulp.task('changes', function (cb) {
     exec(script.changes, function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
+        gulp_util.log(stdout);
+        gulp_util.log(stderr);
         cb(err);
-    })
+    });
 });
 
 gulp.task('build', function (cb) {
     exec(script.build, function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
+        gulp_util.log(stdout);
+        gulp_util.log(stderr);
         cb(err);
-    })
+    });
 });
 
 gulp.task('serve', function (cb) {
+    const address = config.address + ':' + config.port
+    gulp_util.log(address);
+
     exec(script.serve, function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
+        gulp_util.log(stdout);
+        gulp_util.log(stderr);
         cb(err);
-    })
+    });
+    opn('http://' + address);
 });
 
 //gulp.task('deploy', ['build'], function () { // removes custom domain

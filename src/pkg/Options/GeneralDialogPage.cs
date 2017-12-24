dd523c48f7f50f324ee765155e2071518e2c51c3
Yann Duran <yd@live.com.au>
2017-12-25 01:01:45 +1100

    Add MaxBrowserWindows to GeneralDialogPage #456

--- src/pkg/Options/GeneralDialogPage.cs
+++ src/pkg/Options/GeneralDialogPage.cs
@@ -20,11 +20,15 @@ namespace Luminous.TimeSavers.Options
         [Description("Allows the whole set of " + TimeSavers + " features to be turned off together")]
         public bool TimeSaversEnabled { get; set; } = true;
 
+        [Category("Luminous Browser Windows")]
+        [DisplayName("Maximum Windows Open")]
+        [Description("Defines how many Luminous Browser windows can be open at any time")]
+        public int MaxBrowserWindows { get; set; } = 20;
+
         //---
 
         //protected override void OnApply(PageApplyEventArgs e)
         //{
-
         //    base.OnApply(e);
         //}
 

70159539398c529aa952eb1b0e59af6e2edc2902
Yann Duran <yd@live.com.au>
2018-01-07 00:50:53 +1100

    Update 'BrowserWindowViewModel'

--- src/ui/BrowserWindow/BrowserWindow.ViewModel.cs
+++ src/ui/BrowserWindow/BrowserWindow.ViewModel.cs
@@ -1,13 +1,24 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Windows.Input;
+using System.Collections.ObjectModel;
 
 namespace Luminous.TimeSavers.UI.BrowserWindow
 {
     using Luminous.Code.MVVM;
+    using Commands;
 
     public class BrowserWindowViewModel : ViewModelBase
     {
-        public string Url { get; }
+        private string url = "";
+
+        public string Url { get => Get<string>(url); set => Set(url); }
+
+        public string UrlHeader { get => "URL:"; }
+
+        public string UrlWatermark { get => "Enter url"; }
+
         public ObservableCollection<string> Urls { get; }
+
+        public ICommand ClearCommand { get => new ClearCommand(); }
 
         public ObservableCollection<string> GetUrls()
         {

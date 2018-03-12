2d4c03fce2b59e283d484ac6396c345fbbe55736
Yann Duran <yd@live.com.au>
2018-03-12 11:55:32 +1100

    Bump vsix version

--- src/pkg/source.extension.cs
+++ src/pkg/source.extension.cs
@@ -11,7 +11,7 @@ namespace Luminous.TimeSavers
         public const string Name = "Luminous Time Savers";
         public const string Description = @"TimeSavers => { developerProductivity++; }";
         public const string Language = "en-US";
-        public const string Version = "2.2.1.11";
+        public const string Version = "2.2.1";
         public const string Author = "Luminous Software Solutions";
         public const string Tags = "luminous,time savers,time-savers,timesavers,visual studio,productivity,guid,guid generator,guid-generator,yannduran,yd";
     }

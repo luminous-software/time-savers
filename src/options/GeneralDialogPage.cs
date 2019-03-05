52c52ab7e2290cfa7b044ada2990465dde16f6b4
Yann Duran <yd@live.com.au>
2019-03-05 11:32:57 +1100

    feat(General options): add version number
    Implements #662

--- src/options/GeneralDialogPage.cs
+++ src/options/GeneralDialogPage.cs
@@ -12,27 +12,19 @@ namespace Luminous.TimeSavers.Options
     [Guid(Guids.GeneralDialogPageString)]
     public class GeneralDialogPage : DialogPage
     {
-        //***
-        //!!!
-        //!!!
-
         [Category(H1 + TimeSavers)]
         [DisplayName(Enable + Space + TimeSavers)]
         [Description("Allows the whole set of " + TimeSavers + " features to be turned off together")]
         public bool TimeSaversEnabled { get; set; } = true;
 
+        [Category(H1 + TimeSavers)]
+        [DisplayName(Constants.TimeSaversVersion)]
+        [Description("Installed " + TimeSavers + " version")]
+        public string TimeSaversVersion { get; } = Vsix.Version;
+
         [Category(H2 + "Luminous Browser")]
         [DisplayName("Maximum Windows Open")]
         [Description("Defines how many Luminous Browser windows can be open at any time")]
         public int MaxBrowserWindows { get; set; } = 100;
-
-        //---
-
-        //protected override void OnApply(PageApplyEventArgs e)
-        //{
-        //    base.OnApply(e);
-        //}
-
-        //***
     }
 }

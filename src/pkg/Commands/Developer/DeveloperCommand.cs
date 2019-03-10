db2962e24fccb51bc36f3a013374d5a8bca19065
Yann Duran <yd@live.com.au>
2019-03-10 19:29:02 +1100

    refactor(TimeSavers): add DeveloperCommand

---
+++ src/pkg/Commands/Developer/DeveloperCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace Luminous.TimeSavers.Commands.Developer
+{
+    using Luminous.Code.VisualStudio.Packages;
+    using Luminous.TimeSavers.Options;
+
+    internal abstract class DeveloperCommand : TimeSaversCommand
+    {
+        private DeveloperDialogPage _developerOptions;
+
+        protected DeveloperDialogPage DeveloperOptions
+            => _developerOptions ?? (_developerOptions = PackageBase.GetDialogPage<DeveloperDialogPage>());
+
+        protected DeveloperCommand(PackageBase package, int id) : base(package, id)
+        { }
+
+        protected override bool CanExecute
+            => base.CanExecute && DeveloperOptions.DeveloperCommandsEnabled;
+    }
+}

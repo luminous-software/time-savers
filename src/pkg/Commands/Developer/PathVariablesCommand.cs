4e653c02dc74bc56ed8ccf12a6a3a16a1698258e
Yann Duran <yd@live.com.au>
2019-03-10 21:00:16 +1100

    refactor(PathVariablesCommand):
    - inherit from DeveloperCommand
    - add CanExecute override

--- src/pkg/Commands/Developer/PathVariablesCommand.cs
+++ src/pkg/Commands/Developer/PathVariablesCommand.cs
@@ -1,45 +1,34 @@
-﻿using System;
-using Microsoft.VisualStudio.Shell;
-
-using static System.Environment;
+﻿using Microsoft.VisualStudio.Shell;
 
 //FEATURE: 392 Path Variables Window
 
 namespace Luminous.TimeSavers.Commands.Developer
 {
-    using Luminous.Code.Exceptions.ExceptionExtensions;
     using Luminous.Code.VisualStudio.Commands;
     using Luminous.Code.VisualStudio.Packages;
-    using Luminous.TimeSavers.UI.PathVariables;
 
-    internal sealed class PathVariablesCommand : TimeSaversCommand
+    using UI.PathVariables;
+
+    internal sealed class PathVariablesCommand : DeveloperCommand
     {
-        //***
-        //!!!
-
         private PathVariablesCommand(PackageBase package)
             : base(package, PackageIds.PathVariablesCommand)
         { }
 
-        //!!!
-
         public static void Instantiate(PackageBase package)
             => Instantiate(new PathVariablesCommand(package));
 
-        //---
+        protected override bool CanExecute
+            => base.CanExecute && DeveloperOptions.PathVariablesCommandEnabled;
 
         protected override void OnExecute(OleMenuCommand command)
             => ExecuteCommand()
                 .ShowProblem()
                 .ShowInformation();
 
-        //---
-
         private CommandResult ExecuteCommand()
         {
             return Package.ShowToolWindow<PathVariablesToolWindowPane>();
         }
-
-        //***
     }
 }

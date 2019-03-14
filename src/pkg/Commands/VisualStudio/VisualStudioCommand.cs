0b6004b6762d4416e300249be6677b39b84f08e9
Yann Duran <yd@live.com.au>
2019-03-14 16:11:19 +1100

    refactor(commands): add VisualStudioCommand

---
+++ src/pkg/Commands/VisualStudio/VisualStudioCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace Luminous.TimeSavers.Commands.VisualStudio
+{
+    using Luminous.Code.VisualStudio.Packages;
+    using Luminous.TimeSavers.Options;
+
+    internal abstract class VisualStudioCommand : TimeSaversCommand
+    {
+        private VisualStudioDialogPage _visualStudioOptions;
+
+        protected VisualStudioDialogPage VisualStudioOptions
+            => _visualStudioOptions ?? (_visualStudioOptions = PackageBase.GetDialogPage<VisualStudioDialogPage>());
+
+        protected VisualStudioCommand(PackageBase package, int id) : base(package, id)
+        { }
+
+        protected override bool CanExecute
+            => base.CanExecute && VisualStudioOptions.VisualStudioCommandsEnabled;
+    }
+}
